372f30ba467ce82e797b139a7127e28fccd6fe1f
Michael Grissinger <mgrissinger@justfab.com>
2023-07-25 13:04:55 -0400

    Cleanup + more resources

--- src/globals.js
+++ src/globals.js
@@ -1,53 +1,89 @@
-const links = {
-  home: '/',
-  engagement: '/cto-as-a-service',
-  expertise: '/expertise',
-  contact: '/contact',
-  consultation: '/free-consultation',
-  resources: '/resources',
+const pages = {
+  home: {
+    label: 'Meet Michael',
+    path: '/',
+    seo: {
+      title: 'The Fractional CTO — Hire a CTO as a Service',
+      description:
+        'CTO as a Service: Get 100% of the experience from a seasoned technical executive at a fraction of the cost',
+    },
+  },
+  engagement: {
+    label: 'CTO as a Service',
+    path: '/cto-as-a-service',
+    seo: {
+      title: 'The Fractional CTO — Engaging with a CTO as a Service',
+      description: '',
+    },
+  },
+  expertise: {
+    label: 'Expertise',
+    path: '/expertise',
+    seo: {
+      title: 'Product Engineering Expertise &mdash; The Fractional CTO',
+      description: '',
+    },
+  },
+  contact: {
+    label: 'Contact',
+    path: '/contact',
+    seo: {
+      title: 'Contact — The Fractional CTO',
+      description: '',
+    },
+  },
+  consultation: {
+    label: 'Free Consultation',
+    path: '/free-consultation',
+    seo: {
+      title: 'Book a Free Consultation with a Fractional CTO',
+      description: '',
+    },
+  },
 };
 
-const resources = {
-  'how-much-does-a-fractional-cto-cost': {
+const resources = [
+  {
+    path: '/how-much-does-a-fractional-cto-cost',
     seo: {
       title: 'How Much Does a Fractional CTO Cost?',
       description: '',
     },
+    label: 'How Much Does a Fractional CTO Cost?',
   },
-  'what-is-a-fractional-cto': {
+  {
+    path: '/what-is-a-fractional-cto',
     seo: {
       title: 'What is a Fractional CTO? Full Time vs Fractional CTO',
       description: '',
     },
+    label: 'Benefits of a Fractional CTO vs Full Time CTO',
   },
-};
+  {
+    path: '/why-startups-should-hire-a-fractional-cto',
+    seo: {
+      title: 'Fractional CTOs for Startups - Benefits of Hiring a Fractional CTO',
+    },
+    label: 'Hiring a Fractional CTO for Startups',
+  },
+  {
+    path: '/augmented-staffing-vs-hiring-an-agency',
+    seo: {
+      title: 'Augmented Staffing vs Hiring an Agency - CTOs opinion',
+    },
+    label: 'Augmented Staffing vs Hiring an Agency',
+  },
+];
 
 const globals = {
-  nav: [
-    {
-      label: 'Meet Michael',
-      link: links.home,
-    },
-    {
-      label: 'CTO as a Service',
-      link: links.engagement,
-    },
-    {
-      label: 'Expertise',
-      link: links.expertise,
-    },
-    {
-      label: 'Contact',
-      link: links.contact,
-    },
-  ],
-  links,
+  nav: [pages.home, pages.engagement, pages.expertise, pages.contact],
+  pages,
   socials: {
     linkedIn: 'https://linkedin.com/in/michaelgrissinger',
   },
+  resources,
 };
 
 module.exports = {
   globals,
-  resources,
 };
--- src/routes.js
+++ src/routes.js
@@ -1,153 +1,170 @@
 const { Router } = require('express');
 const { resources, globals } = require('./globals');
 const contactFormValidation = require('./middleware/contactFormValidation');
-const {processForm} = require("./utils");
+const { processForm } = require('./utils');
 
 const router = Router();
 
 // Home Page.
-router.get(globals.links.home, (request, response) => {
+router.get(globals.pages.home.path, (request, response) => {
   return response.render('../src/pages/home.hbs', {
     ...globals,
-    nav: globals.nav.map(navItem => {
+    seo: { ...globals.pages.home.seo, url: globals.pages.home.path },
+    nav: globals.nav.map((navItem) => {
       return {
         ...navItem,
-        active: navItem.link === request.path
+        active: navItem.path === request.path,
       };
     }),
-    seo: {
-      title: 'The Fractional CTO - Hire a CTO as a Service',
-      url: globals.links.home,
-      description: 'CTO as a Service: Get 100% of the experience from a seasoned technical executive at a fraction of the cost',
-    },
   });
 });
 
 // CTO as a Service
-router.get(globals.links.engagement, (request, response) => {
+router.get(globals.pages.engagement.path, (request, response) => {
   return response.render('../src/pages/engagement.hbs', {
     ...globals,
-    nav: globals.nav.map(navItem => {
+    seo: { ...globals.pages.engagement.seo, url: globals.pages.engagement.path },
+    nav: globals.nav.map((navItem) => {
       return {
         ...navItem,
-        active: navItem.link === request.path
+        active: navItem.path === request.path,
       };
     }),
-    seo: {
-      title: 'The Fractional CTO - Hire a CTO as a Service',
-      url: globals.links.engagement,
-      description: '',
-    },
   });
 });
 
 // Expertise
-router.get(globals.links.expertise, (request, response) => {
+router.get(globals.pages.expertise.path, (request, response) => {
   return response.render('../src/pages/expertise.hbs', {
     ...globals,
-    nav: globals.nav.map(navItem => {
+    seo: { ...globals.pages.expertise.seo, url: globals.pages.expertise.path },
+    nav: globals.nav.map((navItem) => {
       return {
         ...navItem,
-        active: navItem.link === request.path
+        active: navItem.path === request.path,
       };
     }),
-    seo: {
-      title: 'The Fractional CTO - Hire a CTO as a Service',
-      url: globals.links.expertise,
-      description: '',
-    },
     skills: {
-      backend: ['Javascript', 'TypeScript', 'Node.js', 'Express', 'NestJS', 'Python', 'Django', 'Ruby (on Rails)', 'PHP', 'Laravel', 'Symfony', 'ColdFusion'],
-      database: ['SQL', 'MySQL', 'MariaDB', 'MSSQL', 'Postgres', 'NoSQL', 'mongodb', 'dynamodb', 'couchbase', 'Event Driven Databases', 'Kafka', 'FireStore'],
-      frontend: ['React', 'Next.js', 'Vue', 'Nuxt', 'Angular', 'Javascript', 'TypeScript', 'jQuery'],
-      data: ['ETLs &amp; Data Pipelines', 'Snowflake', 'Redshift', 'Data Lakes', 'Analytics', 'Dashboards'],
+      backend: [
+        'Javascript',
+        'TypeScript',
+        'Node.js',
+        'Express',
+        'NestJS',
+        'Python',
+        'Django',
+        'Ruby (on Rails)',
+        'PHP',
+        'Laravel',
+        'Symfony',
+        'ColdFusion',
+      ],
+      database: [
+        'SQL',
+        'MySQL',
+        'MariaDB',
+        'MSSQL',
+        'Postgres',
+        'NoSQL',
+        'mongodb',
+        'dynamodb',
+        'couchbase',
+        'Event Driven Databases',
+        'Kafka',
+        'FireStore',
+      ],
+      frontend: [
+        'React',
+        'Next.js',
+        'Vue',
+        'Nuxt',
+        'Angular',
+        'Javascript',
+        'TypeScript',
+        'jQuery',
+      ],
+      data: [
+        'ETLs &amp; Data Pipelines',
+        'Snowflake',
+        'Redshift',
+        'Data Lakes',
+        'Analytics',
+        'Dashboards',
+      ],
       mobile: ['iOS (swift)', 'android (java)', 'React Native', 'expo', 'Flutter'],
       infrastructure: ['AWS', 'GCP', 'Containerization', 'Docker', 'Kubernetes', 'CI/CD', 'DevOps'],
       specialties: ['Ecommerce', 'Multi Tenant Software as a Service', 'FinTech'],
       monitoring: ['Cloudwatch', 'Sentry', 'DataDog', 'LogRocket'],
-    }
+    },
   });
 });
 
 // Contact
-router.get(globals.links.contact, (request, response) => {
+router.get(globals.pages.contact.path, (request, response) => {
   return response.render('../src/pages/contact.hbs', {
     ...globals,
-    nav: globals.nav.map(navItem => {
+    seo: { ...globals.pages.contact.seo, url: globals.pages.contact.path },
+    nav: globals.nav.map((navItem) => {
       return {
         ...navItem,
-        active: navItem.link === request.path
+        active: navItem.path === request.path,
       };
     }),
-    seo: {
-      title: 'The Fractional CTO - Hire a CTO as a Service',
-      url: globals.links.contact,
-      description: '',
-    },
     showForm: true,
   });
 });
 
 router.post(
-  globals.links.contact,
+  globals.pages.contact.path,
   contactFormValidation(),
   processForm,
   (request, response) => {
     return response.render('../src/pages/contact.hbs', {
       ...globals,
-      nav: globals.nav.map(navItem => {
+      seo: { ...globals.pages.contact.seo, url: globals.pages.contact.path },
+      nav: globals.nav.map((navItem) => {
         return {
           ...navItem,
-          active: navItem.link === request.path
+          active: navItem.path === request.path,
         };
       }),
-      seo: {
-        title: 'The Fractional CTO - Hire a CTO as a Service',
-        url: globals.links.contact,
-        description: '',
-      },
       showForm: !request.submitSuccess,
       showSuccess: request.submitSuccess,
       formErrors: request.formErrors,
       form: request.body,
     });
-  }
+  },
 );
 
 // Consultation
-router.get(globals.links.consultation, (request, response) => {
+router.get(globals.pages.consultation.path, (request, response) => {
   return response.render('../src/pages/consultation.hbs', {
     ...globals,
-    nav: globals.nav.map(navItem => {
+    seo: { ...globals.pages.consultation.seo, url: globals.pages.consultation.path },
+    nav: globals.nav.map((navItem) => {
       return {
         ...navItem,
-        active: navItem.link === request.path
+        active: navItem.path === request.path,
       };
     }),
-    seo: {
-      title: 'The Fractional CTO - Hire a CTO as a Service',
-      url: globals.links.consultation,
-      description: '',
-    },
   });
 });
 
-Object.keys(resources).forEach(resourceUrl => {
-  router.get(`${globals.links.resources}/${resourceUrl}`, (request, response) => {
-    return response.render(`../src/pages/resources/${resourceUrl}.hbs`, {
+globals.resources.forEach((resource) => {
+  router.get(`/resources${resource.path}`, (request, response) => {
+    return response.render(`../src/pages/resources${resource.path}.hbs`, {
       ...globals,
-      nav: globals.nav.map(navItem => {
+      nav: globals.nav.map((navItem) => {
         return {
           ...navItem,
-          active: navItem.link === request.path
+          active: navItem.path === request.path,
         };
       }),
       seo: {
-        title: resources[resourceUrl].seo.title,
-        url: `${globals.links.resources}/${resourceUrl}`,
-        description: resources[resourceUrl].seo.description,
+        ...resource.seo,
+        url: `/resources${resource.path}`,
       },
-    })
+    });
   });
 });
 
